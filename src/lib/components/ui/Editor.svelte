e4d7fb4ec532aaffcab1d3dca36b957868e8fca1
David Dal Busco <david.dalbusco@outlook.com>
2023-12-01 08:50:01 +0100

    chore: remove comment

--- src/lib/components/ui/Editor.svelte
+++ src/lib/components/ui/Editor.svelte
@@ -13,37 +13,36 @@
 	let element: HTMLElement;
 	let editor: Editor | undefined;
 
-	// https://codesandbox.io/p/sandbox/tiptap-react-ts-simple-text-editor-hcyp3l?file=%2Fsrc%2FLinkModal.tsx
-
-	onMount(async () => {
-		editor = new Editor({
-			element: element,
-			extensions: [
-				StarterKit,
-				Highlight,
-				Typography,
-				Underline,
-				Link.configure({
-					HTMLAttributes: {
-						rel: 'noopener noreferrer'
+	onMount(
+		async () =>
+			(editor = new Editor({
+				element: element,
+				extensions: [
+					StarterKit,
+					Highlight,
+					Typography,
+					Underline,
+					Link.configure({
+						HTMLAttributes: {
+							rel: 'noopener noreferrer'
+						}
+					}).extend({
+						inclusive: false
+					})
+				],
+				content,
+				onTransaction: () => {
+					// force re-render so `editor.isActive` works as expected
+					editor = editor;
+				},
+				editorProps: {
+					attributes: {
+						class:
+							'prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl my-8 mx-5 focus:outline-none'
 					}
-				}).extend({
-					inclusive: false
-				})
-			],
-			content,
-			onTransaction: () => {
-				// force re-render so `editor.isActive` works as expected
-				editor = editor;
-			},
-			editorProps: {
-				attributes: {
-					class:
-						'prose dark:prose-invert prose-sm sm:prose-base lg:prose-lg xl:prose-2xl my-8 mx-5 focus:outline-none'
 				}
-			}
-		});
-	});
+			}))
+	);
 
 	onDestroy(() => editor?.destroy());
 </script>
