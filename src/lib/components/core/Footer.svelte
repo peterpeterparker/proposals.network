851c740929d3b75190c7194f8bf619d739608a0b
David Dal Busco <david.dalbusco@outlook.com>
2023-12-03 21:39:45 +0100

    feat: footer

--- src/lib/components/core/Footer.svelte
+++ src/lib/components/core/Footer.svelte
@@ -1,10 +1,24 @@
+<script>
+	import IconJuno from '$lib/components/icons/IconJuno.svelte';
+</script>
+
 <footer
-	class="w-full h-[200px] m-auto flex justify-between items-center px-5 md:px-24 2xl:p-32 bg-black"
+	class="flex-col lg:flex-row w-full lg:h-[200px] m-auto flex justify-between items-center py-5 px-5 md:px-24 2xl:p-32 bg-black"
 >
-	<div><a class="text-2xl font-bold text-white" href="/">proposals.network.</a></div>
+	<div>
+		<a
+			class="text-2xl font-bold text-white flex items-center gap-3"
+			href="https://juno.build"
+			target="_blank"
+			aria-label="Built with Juno"
+		>
+			<IconJuno />
+			Build with Juno
+		</a>
+	</div>
 	<div>
 		<small class="text-white"
-			>By <a
+			>by <a
 				class="inline-block hover:underline hover:underline-offset-8 mb-4"
 				href="https://daviddalbusco.com"
 				rel="noreferrer noopener"
