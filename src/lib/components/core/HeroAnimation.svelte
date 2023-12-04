9a05500c23145ff50cef48ef9ce7cc57ade6d003
David Dal Busco <david.dalbusco@outlook.com>
2023-12-04 17:33:17 +0100

    feat: hero dash

--- src/lib/components/core/HeroAnimation.svelte
+++ src/lib/components/core/HeroAnimation.svelte
@@ -1,17 +1,27 @@
 <script lang="ts">
 	import IconLogo from '$lib/components/icons/IconLogo.svelte';
+	import IconCircleDashed from '$lib/components/icons/IconCircleDashed.svelte';
 </script>
 
 <div
-	class="border-2 border-black opacity-10 w-[50vw] aspect-square rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-></div>
+	class="opacity-10 w-[50vw] aspect-square rounded-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+>
+	<IconCircleDashed />
+</div>
 
 <div
 	class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[70vw] aspect-square"
 >
 	<div class="animate-spin-slow w-full h-full">
-		<div class="border-2 border-black opacity-20 rounded-full w-full h-full"></div>
-		<div class="absolute bottom-[7vw] right-[7vw]"><IconLogo size="96px" /></div>
+		<div class="border-custom opacity-10 w-full h-full">
+			<IconCircleDashed />
+		</div>
+		<div class="absolute bottom-[7vw] left-[8vw]">
+			<div class="animate-spin-slow"><IconLogo size="96px" /></div>
+		</div>
+		<div class="absolute bottom-[7vw] right-[7vw]">
+			<div class="animate-spin"><IconLogo size="96px" /></div>
+		</div>
 	</div>
 </div>
 
@@ -19,7 +29,11 @@
 	class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[90vw] aspect-square"
 >
 	<div class="animate-spin-slow w-full h-full">
-		<div class="border-2 border-black opacity-30 rounded-full w-full h-full"></div>
-		<div class="absolute top-[10vw] left-[10vw]"><IconLogo size="96px" /></div>
+		<div class="border-custom opacity-10 w-full h-full">
+			<IconCircleDashed />
+		</div>
+		<div class="absolute top-[11vw] left-[11vw]">
+			<div class="animate-spin-slow"><IconLogo size="96px" /></div>
+		</div>
 	</div>
 </div>
---
+++ src/lib/components/icons/IconCircleDashed.svelte
@@ -0,0 +1,3 @@
+<svg viewBox="0 0 100 100">
+    <circle cx="50" cy="50" r="48" stroke-dasharray="6, 2" fill="transparent" stroke="#000000" stroke-width="0.25" />
+</svg>
