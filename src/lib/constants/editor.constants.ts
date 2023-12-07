2c3ac6f4f954c820744caff66de4b0ad0ba44a0d
David Dal Busco <david.dalbusco@outlook.com>
2023-12-07 14:19:38 +0100

    feat: support table in editor

--- src/lib/constants/editor.constants.ts
+++ src/lib/constants/editor.constants.ts
@@ -1,6 +1,10 @@
 import { CodeBlockLowlight } from '@tiptap/extension-code-block-lowlight';
 import Highlight from '@tiptap/extension-highlight';
 import Link from '@tiptap/extension-link';
+import { Table } from '@tiptap/extension-table';
+import { TableCell } from '@tiptap/extension-table-cell';
+import { TableHeader } from '@tiptap/extension-table-header';
+import { TableRow } from '@tiptap/extension-table-row';
 import Typography from '@tiptap/extension-typography';
 import Underline from '@tiptap/extension-underline';
 import StarterKit from '@tiptap/starter-kit';
@@ -24,5 +28,11 @@ export const EDITOR_EXTENSIONS = [
 	Markdown,
 	CodeBlockLowlight.configure({
 		lowlight: createLowlight(common)
-	})
+	}),
+	Table.configure({
+		resizable: true
+	}),
+	TableRow,
+	TableHeader,
+	TableCell
 ];
