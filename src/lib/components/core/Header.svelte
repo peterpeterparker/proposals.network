55ebac0c11176aa51f32e570679aa77c6f28636b
David Dal Busco <david.dalbusco@outlook.com>
2023-12-12 21:59:08 +0100

    feat: nav

--- src/lib/components/core/Header.svelte
+++ src/lib/components/core/Header.svelte
@@ -1,17 +1,22 @@
-<script>
+<script lang="ts">
 	import IconMenu from '$lib/components/icons/IconMenu.svelte';
 	import Menu from '$lib/components/core/Menu.svelte';
 	import IconLogo from '$lib/components/icons/IconLogo.svelte';
 	import SubmitLink from '$lib/components/submit/SubmitLink.svelte';
+	import {governanceIdStore} from "$lib/derived/governance.derived";
+	import {nonNullish} from "@dfinity/utils";
 
 	export let submit = false;
 
 	let visible = false;
+
+	let homeUrl: string;
+	$: homeUrl = `/${nonNullish($governanceIdStore) ? `?g=${$governanceIdStore}` : ""}`
 </script>
 
 <header class="bg-white h-20 w-full fixed top-0 z-10 border-b-2 border-black z-40">
 	<div class="w-full h-full m-auto flex justify-between items-center px-5 md:px-24">
-		<a class="text-2xl font-bold" href="/" aria-label="Proposals.network home"><IconLogo /></a>
+		<a class="text-2xl font-bold" href={homeUrl} aria-label="Proposals.network home"><IconLogo /></a>
 		<nav class="w-1/2 h-full">
 			<ul class="flex justify-end items-center space-x-4 h-full">
 				{#if submit}
