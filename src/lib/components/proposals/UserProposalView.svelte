34893f5a6e0b7448dea81bb631e56ac075585af7
David Dal Busco <david.dalbusco@outlook.com>
2023-12-25 09:59:43 +0100

    feat: user proposal url

--- src/lib/components/proposals/UserProposalView.svelte
+++ src/lib/components/proposals/UserProposalView.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import ButtonCell from '$lib/components/ui/ButtonCell.svelte';
+	import { userProposalUrl } from '$lib/utils/nav.utils';
+	import { governanceIdStore } from '$lib/derived/governance.derived';
 
 	export let key: string;
 
 	let href: string;
-	$: href = `/submit/?key=${key}`;
+	$: href = userProposalUrl({ key, governanceId: $governanceIdStore });
 </script>
 
 <td>
