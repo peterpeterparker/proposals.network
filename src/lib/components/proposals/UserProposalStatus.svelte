cae1e1514f39678a4b5ae8205179616a4e210f90
David Dal Busco <david.dalbusco@outlook.com>
2023-12-27 20:09:41 +0100

    chore: lint

--- src/lib/components/proposals/UserProposalStatus.svelte
+++ src/lib/components/proposals/UserProposalStatus.svelte
@@ -4,7 +4,6 @@
 	import { onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
 	import { getProposal } from '$lib/services/proposal.services';
-	import { governanceTypeStore } from '$lib/derived/governance.derived';
 	import type { Governance } from '$lib/types/governance';
 
 	export let governance: Governance | undefined;
