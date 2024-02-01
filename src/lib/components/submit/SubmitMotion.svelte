24cd9bf930053bd47d74519ae37b62853460559b
louisevelayo <louisevelayo@gmail.com>
2024-02-01 09:58:39 +0100

    add SubmitMotion.svelte

---
+++ src/lib/components/submit/SubmitMotion.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import type { ProposalContent } from '$lib/types/juno';
+    import type { ProposalEditableMetadata } from '$lib/types/juno';
+    import SubmitWriteEdit from '$lib/components/submit/SubmitWriteEdit.svelte';
+    import { getEditable } from '$lib/services/idb.services';
+    import { onMount } from 'svelte';
+
+    let metadata: ProposalEditableMetadata | undefined;
+    let content: ProposalContent | undefined;
+
+    onMount(async () => ([metadata, content] = await getEditable()));
+</script>
+
+<h2 class="mb-6 text-2xl">
+    To submit a proposal, you need both a summary (explaining what it's all about) and motion text
+    (the effective motion for voting), along with a title and a URL pointing to your community
+    discussion.
+</h2>
+
+<SubmitWriteEdit {metadata} {content} />
--- src/lib/components/submit/SubmitWrite.svelte
+++ src/lib/components/submit/SubmitWrite.svelte
@@ -1,34 +1,21 @@
 <script lang="ts">
-	import type { ProposalContent } from '$lib/types/juno';
-	import SubmitContinue from '$lib/components/submit/SubmitContinue.svelte';
-	import { createEventDispatcher, onMount } from 'svelte';
-	import SubmitWriteEdit from '$lib/components/submit/SubmitWriteEdit.svelte';
-	import type { ProposalEditableMetadata } from '$lib/types/juno';
-	import { getEditable } from '$lib/services/idb.services';
+    import SubmitContinue from '$lib/components/submit/SubmitContinue.svelte';
+    import { createEventDispatcher} from 'svelte';
+	import SubmitMotion from '$lib/components/submit/SubmitMotion.svelte';
 
-	let metadata: ProposalEditableMetadata | undefined;
-	let content: ProposalContent | undefined;
+    export let proposalType: string | undefined;
 
-	export let proposalType: string | undefined;
+    console.log("Proposal Type: " + proposalType);
 
-	console.log("Proposal Type: " + proposalType);
+    const dispatch = createEventDispatcher();
+    const next = () => dispatch('pnwrkNext');
 
-	onMount(async () => ([metadata, content] = await getEditable()));
-
-	const dispatch = createEventDispatcher();
-	const next = () => dispatch('pnwrkNext');
-
-	$: console.log(proposalType);
+    $: console.log(proposalType);
 </script>
 
 <h1 class="mb-12 text-4xl font-bold capitalize md:text-6xl">Craft Your Proposal</h1>
 
-<h2 class="mb-6 text-2xl">
-	To submit a proposal, you need both a summary (explaining what it's all about) and motion text
-	(the effective motion for voting), along with a title and a URL pointing to your community
-	discussion.
-</h2>
-
-<SubmitWriteEdit {metadata} {content} />
+<SubmitMotion />
 
 <SubmitContinue on:click={next} />
+
