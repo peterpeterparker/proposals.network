5961cbe00f20d96ec7bfaee9fe24500aefdbe52f
louisevelayo <louisevelayo@gmail.com>
2024-02-14 11:01:33 +0100

    refactor: use tailwind instead of style tag

--- src/lib/components/submit/SubmitSelect.svelte
+++ src/lib/components/submit/SubmitSelect.svelte
@@ -13,19 +13,11 @@
 
 <h1 class="mb-12 text-4xl font-bold capitalize md:text-6xl">Select Proposal Action</h1>
 
-<style>
-    .flex-container {
-      display: flex;
-      gap: 1rem; 
-    }
-</style>
-
-<div class="mb-4 flex-container">
+<div class="flex flex-row mb-4 gap-x-4">
     <ProposalSelector bind:proposalAction={proposalAction} />
     <SubmitContinue on:click={next} />
 </div>
   
-
 <p class="mb-4 leading-relaxed">
 	For detailed information about different proposal types and topics, see the <ExternalLink href="https://internetcomputer.org/docs/current/tokenomics/nns/nns-intro#topics">documentation</ExternalLink>
 </p>
