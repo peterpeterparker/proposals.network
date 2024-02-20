dbc9b5fb23aa3e2198a154f8df5f4f10b762dfbb
louisevelayo <louisevelayo@gmail.com>
2024-02-20 09:24:06 +0100

    refactor: disable continue button if proposalAction is undefined

--- src/lib/components/submit/SubmitSelect.svelte
+++ src/lib/components/submit/SubmitSelect.svelte
@@ -1,25 +1,21 @@
 <script lang="ts">
 	import ExternalLink from '$lib/components/ui/ExternalLink.svelte';
 	import ProposalSelector from '$lib/components/proposals/ProposalSelector.svelte';
-	import SubmitContinue from '$lib/components/submit/SubmitContinue.svelte';
+	import SubmitSelectContinue from '$lib/components/submit/SubmitSelectContinue.svelte';
 	import { createEventDispatcher } from 'svelte';
 	import type { ProposalAction } from '$lib/types/governance';
 
 	export let proposalAction: ProposalAction | undefined;
 
 	const dispatch = createEventDispatcher();
-	const next = () => {
-		if (proposalAction !== undefined) {
-			dispatch('pnwrkNext');
-		}
-	};
+	const next = () => dispatch('pnwrkNext');
 </script>
 
 <h1 class="mb-12 text-4xl font-bold capitalize md:text-6xl">Select Proposal Action</h1>
 
 <div class="flex flex-row mb-4 gap-x-4">
 	<ProposalSelector bind:proposalAction />
-	<SubmitContinue on:click={next} />
+	<SubmitSelectContinue on:click={next} {proposalAction} />
 </div>
 
 <p class="mb-4 leading-relaxed">
---
+++ src/lib/components/submit/SubmitSelectContinue.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/Button.svelte';
+	import type { ProposalAction } from '$lib/types/governance';
+
+	export let proposalAction: ProposalAction | undefined;
+</script>
+
+<Button color="tertiary" disabled={!proposalAction} on:click>Continue</Button>
