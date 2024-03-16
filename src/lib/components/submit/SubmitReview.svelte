71eb8ef39802ba7a0c9bd65d7632148bb0f96b59
louisevelayo <79041287+louisevelayo@users.noreply.github.com>
2024-03-16 15:17:05 +0100

    feat: add review step for node provider registration (#21)
    * add review step for node provider proposals
    
    * refactor: extract components from submit review
    
    add SubmitReviewMotion.svelte
    add SubmitReviewAddNodeProvider.svelte
    
    * chore: lint
    
    * chore: cleanup and add todo
    
    * fix: preserve fade transition in review step
    
    * chore: lint

--- src/lib/components/submit/SubmitReview.svelte
+++ src/lib/components/submit/SubmitReview.svelte
@@ -2,20 +2,24 @@
 	import type { ProposalContent } from '$lib/types/juno';
 	import { createEventDispatcher, onMount } from 'svelte';
 	import { nonNullish } from '@dfinity/utils';
-	import { fade } from 'svelte/transition';
 	import SubmitError from '$lib/components/submit/SubmitError.svelte';
 	import Copy from '$lib/components/ui/Copy.svelte';
 	import { isBusy } from '$lib/derived/busy.derived';
 	import Button from '$lib/components/ui/Button.svelte';
 	import type { ProposalEditableMetadata } from '$lib/types/juno';
 	import { getEditable } from '$lib/services/idb.services';
-	import Container from '$lib/components/ui/Container.svelte';
 	import { submitProposal } from '$lib/services/submit.services';
 	import { userStore } from '$lib/stores/user.store';
-	import HtmlMarkdown from '$lib/components/ui/HtmlMarkdown.svelte';
 	import { governanceStore } from '$lib/derived/governance.derived';
+	import { SUBMIT_CONTEXT_KEY, type SubmitContext } from '$lib/types/submit.context';
+	import { getContext } from 'svelte';
+	import SubmitReviewAddNodeProvider from '$lib/components/submit/SubmitReviewAddNodeProvider.svelte';
+	import SubmitReviewMotion from '$lib/components/submit/SubmitReviewMotion.svelte';
+	import { fade } from 'svelte/transition';
 
 	export let neuronId: string | undefined;
+
+	const { store }: SubmitContext = getContext<SubmitContext>(SUBMIT_CONTEXT_KEY);
 
 	let metadata: ProposalEditableMetadata | undefined;
 	let content: ProposalContent | undefined;
@@ -44,39 +48,29 @@
 </script>
 
 {#if nonNullish(neuronId) && nonNullish(content)}
-	<form on:submit|preventDefault={onSubmit}>
-		<h1 class="font-bold capitalize text-4xl md:text-6xl mb-12">
-			Make sure everything looks good before submitting!
-		</h1>
+	<h1 class="mb-12 text-4xl font-bold capitalize md:text-6xl">
+		Make sure everything looks good before submitting!
+	</h1>
 
-		<p class="leading-relaxed mb-8">
-			Review your proposal for Neuron ID: <Copy value={`${neuronId}`} text="Neuron ID copied." />.
-		</p>
+	<p class="mb-8 leading-relaxed">
+		Review your proposal for Neuron ID: <Copy value={`${neuronId}`} text="Neuron ID copied." />.
+	</p>
 
-		<div in:fade>
-			<Container>
-				<aside slot="title">The proposal title</aside>
-				<article class="p-2.5">{metadata?.title ?? ''}</article>
-			</Container>
+	<!-- TODO: Use the context directly within the component instead of passing the metadata -->
+	<div transition:fade>
+		{#if $store?.metadata?.proposalAction === 'AddOrRemoveNodeProvider'}
+			<SubmitReviewAddNodeProvider metadata={$store?.metadata} />
+		{:else}
+			<form on:submit|preventDefault={onSubmit}>
+				<SubmitReviewMotion {metadata} {content} />
+			</form>
+		{/if}
+	</div>
 
-			<HtmlMarkdown {content} />
-
-			<Container>
-				<aside slot="title">An URL pointing to the forum</aside>
-				<article class="p-2.5">{metadata?.url ?? ''}</article>
-			</Container>
-
-			<Container>
-				<aside slot="title">Your motion text</aside>
-				<article class="p-2.5">{metadata?.motionText ?? ''}</article>
-			</Container>
-		</div>
-
-		<div class="flex gap-2">
-			<Button color="quaternary" type="button" disabled={$isBusy} on:click={edit}>Edit</Button>
-			<Button color="tertiary" type="submit" disabled={$isBusy}>Submit</Button>
-		</div>
-	</form>
+	<div class="flex gap-2">
+		<Button color="quaternary" type="button" disabled={$isBusy} on:click={edit}>Edit</Button>
+		<Button color="tertiary" type="submit" disabled={$isBusy}>Submit</Button>
+	</div>
 {:else}
 	<SubmitError />
 {/if}
---
+++ src/lib/components/submit/SubmitReviewMotion.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import Container from '$lib/components/ui/Container.svelte';
+	import HtmlMarkdown from '$lib/components/ui/HtmlMarkdown.svelte';
+	import type { ProposalEditableMetadata } from '$lib/types/juno';
+	import type { ProposalContent } from '$lib/types/juno';
+
+	export let metadata: ProposalEditableMetadata | undefined;
+	export let content: ProposalContent | undefined;
+</script>
+
+<Container>
+	<aside slot="title">The proposal title</aside>
+	<article class="p-2.5">{metadata?.title ?? ''}</article>
+</Container>
+
+<HtmlMarkdown {content} />
+
+<Container>
+	<aside slot="title">An URL pointing to the forum</aside>
+	<article class="p-2.5">{metadata?.url ?? ''}</article>
+</Container>
+
+<Container>
+	<aside slot="title">Your motion text</aside>
+	<article class="p-2.5">{metadata?.motionText ?? ''}</article>
+</Container>
