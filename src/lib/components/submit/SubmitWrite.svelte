36746abb8be2446ad0cf88a1d465fe4957251d34
David Dal Busco <david.dalbusco@outlook.com>
2024-03-26 18:53:41 +0100

    feat: submit busy "Saving..." when editing node providers (#31)

--- src/lib/components/submit/SubmitWrite.svelte
+++ src/lib/components/submit/SubmitWrite.svelte
@@ -4,12 +4,15 @@
 	import SubmitMotion from '$lib/components/submit/SubmitMotion.svelte';
 	import SubmitAddNodeProvider from '$lib/components/submit/SubmitAddNodeProvider.svelte';
 	import { SUBMIT_CONTEXT_KEY, type SubmitContext } from '$lib/types/submit.context';
+	import SubmitBusy from '$lib/components/submit/SubmitBusy.svelte';
 
 	const { store }: SubmitContext = getContext<SubmitContext>(SUBMIT_CONTEXT_KEY);
 
 	const dispatch = createEventDispatcher();
 	const next = () => dispatch('pnwrkNext');
 </script>
+
+<SubmitBusy />
 
 {#if $store?.metadata?.proposalAction === 'AddOrRemoveNodeProvider'}
 	<SubmitAddNodeProvider />
