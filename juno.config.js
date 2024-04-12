41095622cafc2e220e375f8ed7ddd645976183ec
David Dal Busco <david.dalbusco@outlook.com>
2024-04-12 13:23:16 +0200

    feat: cache resources

--- juno.config.js
+++ juno.config.js
@@ -1,12 +1,24 @@
-import {defineConfig} from '@junobuild/config';
+import { defineConfig } from '@junobuild/config';
 
 /** @type {import('@junobuild/config').JunoConfig} */
 export default defineConfig({
-  satellite: {
-    id: 'gz2uc-6yaaa-aaaal-adgyq-cai',
-    source: 'build'
-  },
-  orbiter: {
-    id: '3iier-sqaaa-aaaal-aczaa-cai'
-  }
+	satellite: {
+		id: 'gz2uc-6yaaa-aaaal-adgyq-cai',
+		source: 'build',
+		storage: {
+			headers: [
+				{
+					source: '**/*.png',
+					headers: [['Cache-Control', 'max-age=604800']]
+				},
+				{
+					source: '**/fonts/**/*',
+					headers: [['Cache-Control', 'public, max-age=31536000']]
+				}
+			]
+		}
+	},
+	orbiter: {
+		id: '3iier-sqaaa-aaaal-aczaa-cai'
+	}
 });
