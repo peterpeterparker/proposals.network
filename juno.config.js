a2189b2f28cfac0e4ee8ca9dcbb88de05100cb1a
David Dal Busco <david.dalbusco@outlook.com>
2024-04-12 13:20:39 +0200

    build: juno config file

---
+++ juno.config.js
@@ -0,0 +1,12 @@
+import {defineConfig} from '@junobuild/config';
+
+/** @type {import('@junobuild/config').JunoConfig} */
+export default defineConfig({
+  satellite: {
+    id: 'gz2uc-6yaaa-aaaal-adgyq-cai',
+    source: 'build'
+  },
+  orbiter: {
+    id: '3iier-sqaaa-aaaal-aczaa-cai'
+  }
+});
