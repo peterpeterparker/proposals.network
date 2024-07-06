c8c2352fca57d63da755f14c1dc4dc100b2a77e6
David Dal Busco <david.dalbusco@outlook.com>
2024-07-06 12:57:17 +0200

    feat: toasts error

--- src/lib/services/sns.services.ts
+++ src/lib/services/sns.services.ts
@@ -1,7 +1,8 @@
+import { toasts } from '$lib/stores/toasts.store';
 import { snsYaml } from '$lib/types/sns';
 import { parse } from 'yaml';
 
-export const assertSnsYaml = async (file: File) => {
+export const assertSnsYaml = async (file: File): Promise<{ result: 'ok' | 'error' }> => {
 	const reader = new FileReader();
 	await new Promise((resolve, reject) => {
 		reader.onload = resolve;
@@ -11,7 +12,35 @@ export const assertSnsYaml = async (file: File) => {
 
 	const dataURL = reader?.result as string;
 
-	const json = parse(dataURL);
+	let json: unknown;
 
-    snsYaml.parse(json);
+	try {
+		json = parse(dataURL);
+	} catch (err: unknown) {
+		toasts.error({
+			msg: { text: 'The Yaml file is invalid. It cannot be parsed to an object.' },
+			err
+		});
+		return { result: 'error' };
+	}
+
+	try {
+		snsYaml.parse(json);
+	} catch (err: unknown) {
+		toasts.error({
+			msg: {
+				text: 'The Yaml file contains invalid data. Verify that all required fields are provided with the expected format.'
+			},
+			err
+		});
+		return { result: 'error' };
+	}
+
+	toasts.show({
+		text: "Your Yaml file is valid. Well done. 👍",
+		level: 'info',
+		duration: 2000
+	});
+
+	return { result: 'ok' };
 };
