5398c5b4063341f44120149fb58f18760391fe96
David Dal Busco <david.dalbusco@outlook.com>
2024-07-12 08:52:32 +0200

    feat: hell

--- HACKING.md
+++ HACKING.md
@@ -59,7 +59,7 @@ dfx canister deposit-cycles 50000000000000 qaa6y-5yaaa-aaaaa-aaafa-cai
 In addition you will need to patch the ICRC-1 Index canister Wasm uploaded in the SNS-Wasm canister because `dfx nns install` is buggy.
 
 ```
-./scripts/download.icrc.sh 
+./scripts/download.icrc.sh
 ./scripts/upload.icrc.mjs
 ```
 
@@ -71,7 +71,9 @@ npm run dev
 
 Finally, open the dapp in your browser at [http://localhost:5173/](http://localhost:5173/).
 
-## Additional tips
+## Additional tips for SNS Proposals
+
+### Decentralizing a dApp
 
 If you would like to test the proposal of SNS locally that is linked to `dappCanisters`, proceed as following:
 
@@ -85,3 +87,17 @@ dfx canister update-settings --add-controller r7inp-6aaaa-aaaaa-aaabq-cai satell
 ```
 
 Finally revert your change in `dfx.json` and use the canister ID that was created for the dapp you would like to decentralize locally.
+
+### Neuron Fund
+
+To create a SNS proposal for a SNS.yaml file that specify `neurons_fund_participation: true` you need to do two things.
+
+1. In NNS dapp, select the neuron that you have created and enable `Participate in neuron's fund`.
+
+2. Then open your terminal run following command to provide that particular neuron maturity by running following script:
+
+```
+./scripts/update.neuron.mjs
+```
+
+> Note that you will need a hotkey. The script is hardcoded to use my dfx PEM file, so you will need to update the path to the file in the `dfx.identity.mjs` script and then add the principal that is printed out as a hotkey for the neuron.
--- scripts/actor.utils.mjs
+++ scripts/actor.utils.mjs
@@ -1,15 +1,15 @@
 import pkgAgent from '@dfinity/agent';
 import { idlFactory } from '../node_modules/@junobuild/admin/declarations/satellite/satellite.factory.did.mjs';
+import { getDfxIdentity } from './dfx.identity.mjs';
+import { initIdentity } from './identity.utils.mjs';
 import { satellite } from './satellite.utils.mjs';
-import {initIdentity} from "./identity.utils.mjs";
 
 const { HttpAgent, Actor } = pkgAgent;
 
 export const getActor = async () => {
-	const { identity, satelliteId, container: host } = satellite;
+	const { satelliteId, container: host } = satellite;
 
-	const agent = new HttpAgent({ identity, fetch, host });
-	await agent.fetchRootKey();
+	const agent = await getAgent();
 
 	return Actor.createActor(idlFactory, {
 		agent,
@@ -18,15 +18,58 @@ export const getActor = async () => {
 };
 
 export const getSnsWasmActor = async (canisterId) => {
-	const {idlFactory} = await import('../node_modules/@dfinity/nns/dist/candid/sns_wasm.idl.mjs');
+	const { idlFactory } = await import('../node_modules/@dfinity/nns/dist/candid/sns_wasm.idl.mjs');
 
-	const identity = initIdentity();
-
-	const agent = new HttpAgent({ identity, fetch, host: "http://localhost:8080" });
-	await agent.fetchRootKey();
+	const agent = await getAgent();
 
 	return Actor.createActor(idlFactory, {
 		agent,
 		canisterId
 	});
+};
+
+export const getGovernanceActor = async (canisterId) => {
+	const { idlFactory } = await import(
+		'../node_modules/@dfinity/nns/dist/candid/governance.idl.mjs'
+	);
+
+	const agent = await getDfxAgent();
+
+	return Actor.createActor(idlFactory, {
+		agent,
+		canisterId
+	});
+};
+
+export const getGovernanceTestActor = async (canisterId) => {
+	const { idlFactory } = await import(
+		'../node_modules/@dfinity/nns/dist/candid/governance_test.idl.mjs'
+	);
+
+	const agent = await getDfxAgent();
+
+	return Actor.createActor(idlFactory, {
+		agent,
+		canisterId
+	});
+};
+
+const getAgent = async () => {
+	const identity = initIdentity();
+
+	const agent = new HttpAgent({ identity, fetch, host: 'http://localhost:8080' });
+	await agent.fetchRootKey();
+
+	return agent;
+};
+
+const getDfxAgent = async () => {
+	const identity = getDfxIdentity();
+
+	console.log('Identity:', identity.getPrincipal().toText());
+
+	const agent = new HttpAgent({ identity, fetch, host: 'http://localhost:8080' });
+	await agent.fetchRootKey();
+
+	return agent;
 };
