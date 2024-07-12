858b7ed70fdefbd4040ef31101356db4479ad7e9
David Dal Busco <david.dalbusco@outlook.com>
2024-07-12 08:52:39 +0200

    chore: format

--- scripts/upload.icrc.mjs
+++ scripts/upload.icrc.mjs
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
+import { hexStringToUint8Array } from '@dfinity/utils';
+import { createHash } from 'crypto';
 import { copyFileSync } from 'node:fs';
-import { getSnsWasmActor } from './actor.utils.mjs';
+import { readFile } from 'node:fs/promises';
 import { join } from 'node:path';
-import {readFile} from "node:fs/promises";
-import {createHash} from "crypto";
-import {hexStringToUint8Array} from "@dfinity/utils";
+import { getSnsWasmActor } from './actor.utils.mjs';
 
 const snsWasmCanisterId = 'qaa6y-5yaaa-aaaaa-aaafa-cai';
 
@@ -45,17 +45,19 @@ const deployIndexCanister = async () => {
 	 * }
 	 */
 
-	const {wasm, hash} = await loadGWasm();
+	const { wasm, hash } = await loadGWasm();
 
 	console.log(`Upload ${hash}`);
 
 	await actor.add_wasm({
 		hash: hexStringToUint8Array(hash),
-		wasm: [{
-			wasm,
-			proposal_id: [],
-			canister_type: 6
-		}]
+		wasm: [
+			{
+				wasm,
+				proposal_id: [],
+				canister_type: 6
+			}
+		]
 	});
 
 	// dfx canister call qaa6y-5yaaa-aaaaa-aaafa-cai get_latest_sns_version_pretty
