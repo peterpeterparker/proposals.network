b1a02f6ca829b4c7b75a18f2ea52ed2bdd1f6ef2
David Dal Busco <david.dalbusco@outlook.com>
2024-07-19 12:56:19 +0200

    feat: settings

---
+++ src/lib/components/settings/Settings.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import SplitPane from '$lib/components/ui/SplitPane.svelte';
+	import UserInitializedGuard from '$lib/components/guards/UserInitializedGuard.svelte';
+	import { userNotSignedIn } from '$lib/derived/user.derived';
+    import SettingsSignIn from "$lib/components/settings/SettingsSignIn.svelte";
+	import SubmitTitle from "$lib/components/submit/SubmitTitle.svelte";
+</script>
+
+<SplitPane col="reverse">
+	<UserInitializedGuard>
+		{#if $userNotSignedIn}
+            <SettingsSignIn />
+		{:else}
+			<SubmitTitle>Your Settings</SubmitTitle>
+		{/if}
+	</UserInitializedGuard>
+</SplitPane>
---
+++ src/routes/(split)/settings/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Section from '$lib/components/ui/Section.svelte';
+    import Settings from "$lib/components/settings/Settings.svelte";
+</script>
+
+<Section wide>
+    <Settings />
+</Section>
