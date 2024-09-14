c74b21951fadafe6c080f57a340b0217100982ef
David Dal Busco <david.dalbusco@outlook.com>
2024-09-14 17:07:05 +0200

    chore: clippy

--- src/satellite/src/lib.rs
+++ src/satellite/src/lib.rs
@@ -6,7 +6,7 @@ use junobuild_satellite::{include_satellite, AssertDeleteDocContext, get_doc_sto
 fn assert_delete_doc(context: AssertDeleteDocContext) -> Result<(), String> {
     let metadata = get_doc_store(id(), "metadata".to_string(), context.data.key)?;
 
-    if let Some(_) = metadata {
+    if metadata.is_some() {
         return Err("The document cannot not be deleted. The related metadata still exists.".to_string());
     }
 
