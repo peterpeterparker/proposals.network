f968347f778087d41bc98ce6e32f4f3649314ae7
David Dal Busco <david.dalbusco@outlook.com>
2024-09-14 16:36:07 +0200

    feat: remove assert_set_doc as well

--- src/satellite/src/lib.rs
+++ src/satellite/src/lib.rs
@@ -1,10 +1,5 @@
-use junobuild_macros::{assert_delete_doc, assert_set_doc};
-use junobuild_satellite::{include_satellite, AssertDeleteDocContext, AssertSetDocContext};
-
-#[assert_set_doc]
-fn assert_set_doc(_context: AssertSetDocContext) -> Result<(), String> {
-    Ok(())
-}
+use junobuild_macros::{assert_delete_doc};
+use junobuild_satellite::{include_satellite, AssertDeleteDocContext};
 
 #[assert_delete_doc]
 fn assert_delete_doc(_context: AssertDeleteDocContext) -> Result<(), String> {
