ce4a30d8c422840ed1cc0ee432abba5a69268d5c
David Dal Busco <david.dalbusco@outlook.com>
2025-01-29 14:12:42 +0100

    feat: debug

--- src/lib/components/core/Aside.svelte
+++ src/lib/components/core/Aside.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
 	import UserProposalsLoader from '$lib/components/proposals/UserProposalsLoader.svelte';
 	import GovernanceSubmitLink from '$lib/components/governance/GovernanceSubmitLink.svelte';
+
+	interface Props {
+		cta?: boolean;
+	}
+
+	let { cta = true }: Props = $props();
 </script>
 
 <UserProposalsLoader>
 	<div class="flex-col px-4 md:px-0 hidden lg:flex">
-		<span class="text-lg font-bold block mb-4">You miss a 100% of the shots you don't take.</span>
+		{#if cta}
+			<span class="text-lg font-bold block mb-4">You miss a 100% of the shots you don't take.</span>
 
-		<GovernanceSubmitLink color="quaternary" />
+			<GovernanceSubmitLink color="quaternary" />
+		{/if}
 	</div>
 </UserProposalsLoader>
